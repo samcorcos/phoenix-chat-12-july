47ca040562f7876224b286eb476408edaf3b1ec4
Sam Corcos <sam@corcos.io>
2016-07-12 23:47:56 -0400

    successful unit tests

---
+++ .test-setup.js
@@ -0,0 +1,9 @@
+const noop = () => {}
+const empty = () => ({})
+
+require.extensions['.css'] = empty
+require.extensions['.scss'] = empty
+require.extensions['.ico'] = noop
+require.extensions['.png'] = noop
+require.extensions['.jpg'] = noop
+require.extensions['.svg'] = noop
